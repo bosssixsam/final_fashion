fdbc539124e53d60ef228a8d3aa119bacc92fb82
Quang <quangwk2702@gmail.com>
2022-07-01 03:00:06 +0700

    hoan thien bai cuoi ky

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+
+import Layouts from "@components/layouts/Layouts";
+
+import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layouts>
+      <Component {...pageProps} />
+    </Layouts>
+  );
 }
 
-export default MyApp
+export default MyApp;
